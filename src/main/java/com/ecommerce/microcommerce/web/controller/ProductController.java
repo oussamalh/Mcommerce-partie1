9eeaf00f82ecfd27ee53b5caa11c6cfb9323e135
AIT LHOUARI Oussama <slawii93@gmail.com>
2019-11-30 23:23:41 +0100

    all parts

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -1,22 +1,36 @@
 package com.ecommerce.microcommerce.web.controller;
 
+import java.net.URI;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.json.MappingJacksonValue;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
-import java.net.URI;
-import java.util.List;
 
 
 @Api( description="API pour es opérations CRUD sur les produits.")
@@ -26,6 +40,7 @@ public class ProductController {
 
     @Autowired
     private ProductDao productDao;
+	
 
 
     //Récupérer la liste des produits
@@ -69,6 +84,8 @@ public class ProductController {
 
     public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
 
+        if(product.getPrix()==0) throw new ProduitGratuitException("Le prix du produit que vous essayez d'ajouter est egal à 0.");
+
         Product productAdded =  productDao.save(product);
 
         if (productAdded == null)
@@ -103,6 +120,22 @@ public class ProductController {
         return productDao.chercherUnProduitCher(400);
     }
 
+    @GetMapping(value = "/AdminProduits")
+    public Map<Product, Integer> calculerMargeProduit(){
+    	Iterable<Product> produits = productDao.findAll();
+        Map<Product,Integer> products = new LinkedHashMap<>();
+        produits.iterator().forEachRemaining(p -> products.put(p, (p.getPrix()-p.getPrixAchat())));
+        return products;
+    }
+    
+    @GetMapping(value = "/ProduitsTriee")
+    public Iterable<Product> trierProduitsParOrdreAlphabetique(){
+    	
+    	Iterable<Product> produits = productDao.findByOrderByNom();
+    	 
+
+         return produits;
+    }
 
 
 }
